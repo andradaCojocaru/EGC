50f05905e091a3f3286449ce69bfd8875ad0d424
andradaCojocaru <74014780+andradaCojocaru@users.noreply.github.com>
2022-11-06 18:54:32 +0200

    Add files via upload

---
+++ tema1/Tema1.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "components/simple_scene.h"
+
+
+namespace m1
+{
+    class Tema1 : public gfxc::SimpleScene
+    {
+     public:
+         Tema1();
+        ~Tema1();
+
+        void Init() override;
+
+     private:
+        void FrameStart() override;
+        void Update(float deltaTimeSeconds) override;
+        void FrameEnd() override;
+
+        void OnInputUpdate(float deltaTime, int mods) override;
+        void OnKeyPress(int key, int mods) override;
+        void OnKeyRelease(int key, int mods) override;
+        void OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY) override;
+        void OnMouseBtnPress(int mouseX, int mouseY, int button, int mods) override;
+        void OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods) override;
+        void OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY) override;
+        void OnWindowResize(int width, int height) override;
+        void RenderDuck();
+        void CreateDuck();
+
+     protected:
+        float cx, cy;
+        glm::mat3 modelMatrix;
+        float translateX, translateY;
+        float scaleX, scaleY;
+        float angularStep;
+        int moveUp = 1;
+        float speed = 150;
+        float speed_Initial = 150;
+        float ratioScale = 1.2;
+        float square = 100;
+
+        int up = 1;
+        int change = 0;
+        bool isDuckFirstRendered = 0;
+        int lifes = 3;
+        int bullets = 3;
+        float time = 0;
+        float timeOutside = 0;
+        bool escape = 0;
+        bool dead = 0;
+        int centerX = 10;
+        int centerY = 10;
+        int isHit = 0;
+
+
+        bool start = 0;
+        int number_Ducks = 1;
+        int score = 0;
+        float angle = M_PI_4;
+        //float angle = 0;
+
+        int clickX;
+        int clickY;
+
+        int height;
+        int width;
+
+        glm::vec3 hitBoxLow = glm::vec3(0, 0, 1);
+        // TODO(student): If you need any other class variables, define them here.
+
+    };
+}   // namespace m1
